c571000db63538a8b5cc4dcee7bdf31c20fc3a05
CSuperman <crlbusiness1@gmail.com>
2023-10-28 19:41:40 -0400

    Create problem3.js
    solution without base case

---
+++ problem3.js
@@ -0,0 +1,29 @@
+function wtrapped(height) {
+  
+// implement zero base case
+
+    const num = height.length;
+  
+    const leftMax = new Array(num)
+    const rightMax = new Array(num)
+
+    leftMax[0] = height[0]
+  
+    for (let indx = 1; indx < num; indx++) {
+        leftMax[indx] = Math.max(leftMax[indx - 1], height[indx])
+    }
+
+    rightMax[num - 1] = height[num - 1];
+  
+    for (let indx = num - 2; indx >= 0; indx--) {
+        rightMax[indx] = Math.max(rightMax[indx + 1], height[indx])
+    }
+
+    let waters = 0
+  
+    for (let ind = 0; indx < num; indx++) {
+        waters += Math.min(leftMax[indx], rightMax[indx]) - height[indx]
+    }
+
+    return waters;
+};
