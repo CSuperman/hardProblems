856257843cb75cc83549f5a01b76e1ccfc4c19de
CSuperman <crlbusiness1@gmail.com>
2023-10-29 16:08:31 -0400

    Update problem4.js
    readability

--- problem4.js
+++ problem4.js
@@ -4,11 +4,12 @@ function palindrome_doubles(chars) {
   for (let i1 = 0; i1 < chars.length; i1++) {
     for (let i2 = 0; i2 < chars.length; i2++) {
       if (i1 != i2) {
-        var x = chars[i1] + chars[i2]
-        var y = x.split("").reverse().join("")
+        var x = chars[i1] + chars[i2]            // pairing the strings
+        var y = x.split("").reverse().join("")   // setup for equality check
 
-        if (x === y)
+        if (x === y) {                           // comparing palindrome(s) condition
           result.push([i1, i2])
+        }
       }
     }
   }
