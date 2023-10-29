0a9f694f7292c9dd8d0f8358802b51a76f564e6d
CSuperman <crlbusiness1@gmail.com>
2023-10-29 16:02:59 -0400

    Update problem4.js
    debugging and readability

--- problem4.js
+++ problem4.js
@@ -1,16 +1,20 @@
-function palindrome_doubles(words) {
-  var result = []
+function palindrome_doubles(chars) {
+  var results = []
 
-  for(let i1 = 0; i1 < words.length; i1++) {
-    for(let i2 = 0; i2 < words.length; i2++) {
-      if(i1 != i2) {
-        var x = words[i1] + words[i2]
-        var y = x.split().reverse().join()
+  for (let i1 = 0; i1 < chars.length; i1++) {
+    for (let i2 = 0; i2 < chars.length; i2++) {
+      if (i1 != i2) {
+        var x = chars[i1] + chars[i2]
+        var y = x.split("").reverse().join("")
 
-        if(x === y)
+        if (x === y)
           result.push([i1, i2])
       }
     }
-
-  return result
+  }
+  
+  return results
 }
+
+
+// tests
