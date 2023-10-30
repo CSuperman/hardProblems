738aa3bbe001cebec5f8892fdd304b236196495d
CSuperman <crlbusiness1@gmail.com>
2023-10-30 16:39:49 -0400

    Update problem5.py
    bug

--- problem5.py
+++ problem5.py
@@ -1,11 +1,14 @@
+
 class Node(object):
   def __init__(self, val=0, next=None):
     self.val = value
     self.next = next
 
+# removing the nth node from the end
+
 class Answer(object)
   def Nthend(self, head, nth):
-    point1 = point2, head
+    point1 = point2 = head  # fast and slow pointers
     for _ in range(nth):
       point1 = point1.next
     if not point1:
