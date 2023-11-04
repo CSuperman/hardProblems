c750fe54696fd0e12a34057876e172addefbb363
CSuperman <crlbusiness1@gmail.com>
2023-11-04 00:04:15 -0400

    Update problem8.py
    several bugs fixed

--- problem8.py
+++ problem8.py
@@ -1,22 +1,23 @@
 # example input
 rooms = [[1],[2],[3],[]]
 
-def KeysforRooms(self, rooms: List[List[int]]) -> bool:
-        Roomz=len(rooms)
-        visitList=[1]*roomz
-        que=[0]
-        while que:
-            queued=que.pop(0)
-            if visitList[c]==1:
-                queuedRooms=rooms[queued]
-                que.extend(queued)
-            visitList[c]=0
-        if sum(visitList)==0:
-            print("Yes, you can visit all rooms")
-            return True
-        else:
-            print("No, you cannot visit all rooms")
-            return False
+class Answer:
+  def KeysforRooms(self, rooms: List[List[int]]) -> bool:
+      Roomz=len(rooms)
+      visitList=[1]*Roomz
+      que=[0]
+      while que:
+          queued=que.pop(0)
+          if visitList[queued]==1:
+              queuedRooms=rooms[queued]
+              que.extend(queuedRooms)
+          visitList[queued]=0
+      if sum(visitList)==0:
+          print("Yes, you can visit all rooms")
+          return True
+      else:
+          print("No, you cannot visit all rooms")
+          return False
 
 KeysforRooms(self, rooms)
 # example output: true
