c9cb06728a712363cd6db7ed43d1500db5b0d5ce
CSuperman <crlbusiness1@gmail.com>
2023-11-15 23:57:59 -0500

    Update problem13.py

--- problem13.py
+++ problem13.py
@@ -1,13 +1,17 @@
 def canVisitAllRooms(rooms):
-    visited = set()
-    stack = [0]
+  visited = set()
+  stack = [0]
 
-    while stack:
-        room = stack.pop()
-        visited.add(room)
+  while stack:
+    room = stack.pop()
+    visited.add(room)
 
-        for key in rooms[room]:
-            if key not in visited and key not in stack:
-                stack.append(key)
+    for key in rooms[room]:
+        if key not in visited and key not in stack:
+            stack.append(key)
 
-    return len(visited) == len(rooms)
+  return len(visited) == len(rooms)
+
+
+print(canVisitAllRooms(rooms = [[1],[2],[3],[]]))         #output: true
+print(canVisitAllRooms(rooms = [[1,3],[3,0,1],[2],[0]]))  # output: false
