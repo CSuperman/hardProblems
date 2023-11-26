dab1d18e88e11cf09b9943d81947179d2d03097a
CSuperman <crlbusiness1@gmail.com>
2023-11-25 20:32:36 -0500

    Create problem13.js

---
+++ problem13.js
@@ -0,0 +1,37 @@
+function initializeArray(m, n) {
+    return Array(m).fill().map(() => Array(n).fill());
+}
+
+function isValidAndUndefined(x, y, m, n, result) {
+    return x >= 0 && y >= 0 && x < m && y < n && result[x][y] === undefined;
+}
+
+var updateMatrix = function (mat) {
+    const m = mat.length;
+    const n = mat[0].length;
+    const result = initializeArray(m, n);
+    const queue = [];
+    const directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]; // up, left, down, right
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 0) {
+                result[i][j] = 0;
+                queue.push([i, j]);
+            }
+        }
+    }
+
+    while (queue.length) {
+        const [x, y] = queue.shift();
+        for (const [dx, dy] of directions) {
+            const newX = x + dx;
+            const newY = y + dy;
+            if (isValidAndUndefined(newX, newY, m, n, result)) {
+                queue.push([newX, newY]);
+                result[newX][newY] = result[x][y] + 1;
+            }
+        }
+    }
+    return result;
+};
