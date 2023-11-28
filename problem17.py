d950804d52728d82befbf779c835228360f63864
CSuperman <crlbusiness1@gmail.com>
2023-11-27 21:23:53 -0500

    Create problem17.py
    # python version of the problem 17 solution

---
+++ problem17.py
@@ -0,0 +1,15 @@
+# python version of the problem 17 solution
+
+def makeChange(coins, amount):
+    coins.sort(reverse=True)
+    coinTotal = 0
+    i = 0
+    while amount > 0:
+        if coins[i] <= amount:
+            amount -= coins[i]
+            coinTotal += 1
+        else:
+            i += 1
+    return coinTotal
+
+print(makeChange([5, 10, 25], 50))
