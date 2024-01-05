6119fa796b0ee162fdcea632c01bfccd46eb863c
CSuperman <crlbusiness1@gmail.com>
2024-01-04 23:04:57 -0500

    Create practical4.ts

---
+++ practical4.ts
@@ -0,0 +1,24 @@
+function findPeakElement(nums: number[]): number {
+
+  var r = nums.length - 1	// right
+  var l = 0			// left
+
+
+  while(l < r) {
+    var mid = Math.floor(l + (r - l) / 2)
+
+    if (nums[mid] < nums[mid+1]) {
+      l = mid + 1
+    } 
+
+    else {
+      r = mid
+    }
+  }
+
+  return l
+};
+
+
+const nums = [1, 2, 3, 1]
+console.log(findPeakElement(nums))	// output: 2
